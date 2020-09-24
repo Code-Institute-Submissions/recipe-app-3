628d3e9663d2ec72b315de00ea86b27e4c73bca7
aavasylenko <aavasylenko@hotmail.com>
2020-09-24 01:14:07 +0000

    Added dynamic inputs for ingredients

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
 if os.path.exists("env.py"):
     import env
 
@@ -24,6 +25,36 @@ def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
+
+
+@app.route('/add_recipe', methods=["GET", "POST"])
+def add_recipe():
+    if request.method == "POST":
+        if 'image' in request.files:
+            image = request.files['image']
+            if mongo.db.recipes.find_one({"img": image.filename}):
+                image.filename = uuid.uuid4().hex[:6].upper()
+            mongo.save_file(image.filename, image)
+            recipe = {
+                "name": request.form.get("name"),
+                "recipe_category": request.form.get("recipe_category"),
+                "diet_type": request.form.get("diet_type"),
+                "ingredients": request.form.getlist('ingredient'),
+                "cooking_directions": request.form.getlist('cooking_directions'),
+                "link": request.form.get("link"),
+                "img": image.filename
+            }
+            mongo.db.recipes.insert_one(recipe)
+            flash("Recipe Successfully Added")
+            return redirect(url_for('get_recipes'))
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    diet_types = mongo.db.diet_types.find().sort("diet_name", 1)
+    return render_template('add_recipe.html', categories=categories, diet_types=diet_types)
+
+
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge:"right"});
     $('.modal').modal();
+    $('select').formSelect();
     });
+
+
+
+let ingredientCounter = 1;
+let ingredientLimit = 20;
+function addInput(divId){
+     if (ingredientCounter == ingredientLimit)  {
+          alert("You have reached the limit of adding " + ingredientCounter + " ingredients");
+     }
+     else {
+          var newInputDiv = document.createElement('div');
+          newInputDiv.classList.add("input-field");
+          newInputDiv.innerHTML = "<label for='ingredient'>Ingedient " + (ingredientCounter + 1)+ "</label>" + " <input id='ingredient' name='ingredient' type='text' />";
+          document.getElementById(divId).appendChild(newInputDiv);
+          ingredientCounter++;
+     }
+}
